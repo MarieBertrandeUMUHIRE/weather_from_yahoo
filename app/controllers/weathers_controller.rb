64405df0b69c1f8fa10f9daf47f7b4ca88e9f91b
MarieBertrandeUMUHIRE <mariebertrande.umuhire@gmail.com>
2017-02-14 13:51:03 +0200

    This app can be used to check the current weather condition

---
+++ app/controllers/weathers_controller.rb
@@ -0,0 +1,5 @@
+class WeathersController < ApplicationController
+  def index
+    @weathers = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22kigali%2C%20rwanda%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+  end
+end
